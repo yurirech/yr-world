94cf14d7126e06d5b82d27d0f6a137785c62b3f1
Yuri Nunes <yuri.nunes@tuntscorp.com>
2020-04-21 16:32:28 +0200

    Updates

---
+++ precache-manifest.dc62fb75e632a4e30768db3aa3d6748a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e435e119a5e11e1718f114e5abd480aa",
+    "url": "/yr-world/index.html"
+  },
+  {
+    "revision": "fed91f6b0815926ff3a3",
+    "url": "/yr-world/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4bca22e299b1e0c91cb3",
+    "url": "/yr-world/static/css/main.b0430d00.chunk.css"
+  },
+  {
+    "revision": "fed91f6b0815926ff3a3",
+    "url": "/yr-world/static/js/2.c63bb9e4.chunk.js"
+  },
+  {
+    "revision": "5a733b4f72d20e4afb54e249ef308592",
+    "url": "/yr-world/static/js/2.c63bb9e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bca22e299b1e0c91cb3",
+    "url": "/yr-world/static/js/main.2ef1661e.chunk.js"
+  },
+  {
+    "revision": "dba00c56471c6d24c1cc",
+    "url": "/yr-world/static/js/runtime-main.3560b86a.js"
+  },
+  {
+    "revision": "f8e1b363908bbfc3087a67fda57fdeba",
+    "url": "/yr-world/static/media/bg-test.f8e1b363.jpg"
+  },
+  {
+    "revision": "2cef0be42de3301ab6ea4ee64f0c4ca0",
+    "url": "/yr-world/static/media/main-bg-mobile.2cef0be4.jpg"
+  },
+  {
+    "revision": "b806af82db31c3fabb92c52ce12abff1",
+    "url": "/yr-world/static/media/profile-pic.b806af82.jpeg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/yr-world/precache-manifest.dc62fb75e632a4e30768db3aa3d6748a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yr-world/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
